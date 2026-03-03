4c33bfbfc4c8ccea130995f5520adc9392f35340
hubligpt5-hub <hubligpt5@gmail.com>
2026-03-03 10:40:14 +0530

    Add files via upload

---
+++ search.java
@@ -0,0 +1,10 @@
+class search
+{
+public static void main(String a[])
+int [] num=new num[5];
+for (int i=0;i<=num.length;i++)
+{a[i]=i;}
+for (int i=0;i<=num.length;i++)
+{System.out.println(a[i]=i);
+}
+}
